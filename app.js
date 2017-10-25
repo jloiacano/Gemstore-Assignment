3170d66536a160cedd4d922044208d27b0454cac
jloiacano <j.loiacano55@gmail.com>
2017-10-25 09:48:50 -0500

    start of day 10-25

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
+var gems = [
+    {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Some gems have hidden qualities beyond their lustre, beyond their shine... Dodecahedron is one of those gems',
+        canPurchase: true,
+        outOfStock: false,
+        image: 'images/gem-01.gif'
+    },
+    {
+        name: "Pentagonal Gem",
+        price: 5.95,
+        description: "The origin of the pentagonal gem is unknown.",
+        canPurchase: true,
+        outOfStock: true,
+        image: 'images/gem-02.gif'
+    }
+];
+
 // Module declaration. creating a custom module by the name (in this case myApp) which can be used on the page. Passing 2 args to the module creates a new module as opposed to fetching a built in module.
 var app = angular.module("myApp", []); //similar to C# main function
 
 //Hanging a controller off of the "parent" module. Can also hang services, filters, directives.... same rules as parent module: two args creates custom, 1 arg tries to fetch built in.
-app.controller("myController", function() {
-    window.console.log("myController run");
+app.controller("myController", function ($scope) {
+    $scope.Initialized = function () {
+        $scope.models = gems;
+    }
+});
 
-    this.Initialize = function() {
-        console.log("myController Initialized");
-    };
-})
+app.controller("panelController", function ($scope) {
+    $scope.SelectTab = function (tab) {
+        $scope.tab = tab;
+    }
+});
 
-app.directive("myDirective", function() {
+app.directive("storePanels", function(){
     var directiveObject = {
-        template: "<div>This is a template directive</div>"
+        templateUrl: "templates/store-panels.html",
+        controller: function($scope){
+            $scope.SelectTab = function(tab){
+                $scope.tab = tab;
+            }
+        }
     };
     return directiveObject;
-})
+});
 
-
-app.directive("myOtherDirective", function() {
+app.directive("storeReviews", function(){
     var directiveObject = {
-        templateUrl: "template.html"
+        templateUrl: "templates/store-reviews.html",
     };
     return directiveObject;
-})
+});
