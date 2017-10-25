cc86b0ec1517968da253797e02f8f62063ca43a1
jloiacano <j.loiacano55@gmail.com>
2017-10-25 12:40:35 -0500

    lunchtime commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/*global angular*/
+
 (function () {
     "use strict";
 
+    //For now, just hard-coding a model!
     var gems = [
         {
             name: 'Dodecahedron',
@@ -11,43 +14,33 @@
             image: 'images/gem-01.gif',
             reviews: [
                 {
+                    stars: 2,
+                    author: 'ripalp@codingtemple',
+                    body: 'I hate this product!'
+                },
+                {
                     stars: 5,
-                    author: "j.loiacano55@gmail.com",
-                    body: "I love this product!!!"
+                    author: 'josephj@codingtemple',
+                    body: 'I love this product!'
                 }
             ]
-    },
-        {
+        }, {
             name: "Pentagonal Gem",
             price: 5.95,
             description: "The origin of the pentagonal gem is unknown.",
             canPurchase: true,
             outOfStock: true,
-            image: 'images/gem-02.gif',
-            reviews: [
-                {
-                    stars: 1,
-                    author: "byrd55@aol.com",
-                    body: "what a piece of junk!"
-                }
-            ]
-    }
-];
-
-    // Module declaration. creating a custom module by the name (in this case myApp) which can be used on the page. Passing 2 args to the module creates a new module as opposed to fetching a built in module.
-    var app = angular.module("myApp", []); //similar to C# main function
-
-    //Hanging a controller off of the "parent" module. Can also hang services, filters, directives.... same rules as parent module: two args creates custom, 1 arg tries to fetch built in.
+            image: 'images/gem-02.gif'
+        }
+    ];
+    //Module declaration - I'm creating a new custom module called "myApp" which I can now use on the page. If I pass one argument to the module method, I'm "Fetching" an existing module. If I pass two arguments to the module method, I'm creating a new module.  In this instance, I'm saving the app module to a variable as I'll reuse it often throughout the page
+    var app = angular.module("myApp", []);
+    //Once I have a reference to the app module, I can add other services, controllers, filters, and directives to it using built-in functions.  In this case, passing two arguments to the controller method will create a new controller
     app.controller("myController", function ($scope) {
         $scope.Initialized = function () {
             $scope.models = gems;
-        }
-    });
+        };
 
-    app.controller("panelController", function ($scope) {
-        $scope.SelectTab = function (tab) {
-            $scope.tab = tab;
-        }
     });
 
     app.directive("storePanels", function () {
@@ -56,7 +49,7 @@
             controller: function ($scope) {
                 $scope.SelectTab = function (tab) {
                     $scope.tab = tab;
-                }
+                };
             }
         };
         return directiveObject;
@@ -73,16 +66,46 @@
                     }
                     product.reviews.push($scope.newReview);
 
-                    // clears out newReview for next use
+                    //This resets the review
                     $scope.newReview = {};
-                }
+
+                    $scope.reviewForm.$setPristine();
+                };
+
+                $scope.StarsInvalid = function () {
+                    return $scope.reviewForm.stars.$invalid && $scope.reviewForm.stars.$dirty;
+                };
+
+                $scope.BodyInvalid = function () {
+                    return $scope.reviewForm.body.$invalid && $scope.reviewForm.body.$dirty;
+                };
+
+                $scope.AuthorInvalid = function () {
+                    return $scope.reviewForm.author.$invalid && $scope.reviewForm.author.$dirty;
+                };
+
+                $scope.FormInvalid = function () {
+                    return $scope.StarsInvalid() || $scope.BodyInvalid() || $scope.AuthorInvalid();
+                };
             }
         };
         return directiveObject;
     });
 
 
-
-
+    //I can also attach directives to modules:
+    //app.directive("myDirective", function(){
+    //    var directiveObject = {
+    //        template: "<div>This is a template directive.</div>"
+    //    };
+    //    return directiveObject;
+    //});
+    //A better template directive
+    //app.directive("myOtherDirective", function(){
+    //    var directiveObject = {
+    //        templateUrl: "template.html"
+    //    };
+    //    return directiveObject;
+    //});
 
 }());
